1791e55740812295ea8421a45b1d47baf1a15541
Sonia Mangane <92125231+SoniaMGN@users.noreply.github.com>
2026-03-08 19:52:17 +0100

    updated configurations for render

--- app/db/supabase_client.py
+++ app/db/supabase_client.py
@@ -3,7 +3,7 @@ from app.core.config import settings
 
 supabase = None
 
-if settings.SUPABASE_URL and settings.SUPABASE_SERVICE_KEY:
+if settings.SUPABASE_URL and settings.SUPABASE_SERVICE_ROLEKEY:
     supabase = create_client(
         settings.SUPABASE_URL,
         settings.SUPABASE_SERVICE_KEY
--- app/main.py
+++ app/main.py
@@ -1,18 +1,38 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.router import api_router
-from fastapi.security import HTTPBearer
+from app.db.database import engine, Base #
 
+# This ensures your SQLAlchemy models are recognized, 
+# even though the tables are created manually.
+Base.metadata.bind = engine 
 
 app = FastAPI(
-    title="Local Events API",
+    title="E-VENT Orchestrator",
     version="1.0.0"
 )
 
-security = HTTPBearer() 
+# Without this, the Vercel frontend will be blocked from talking to Render.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # In production, replace "*" with the Vercel URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+# Include  routes
 app.include_router(api_router, prefix="/api")
-
 
 @app.get("/")
 def root():
-    return {"message": "Backend is running"}
+    return {"message": "E-VENT Backend is running on Render"}
+
+@app.get("/")
+def read_root():
+    return {"status": "E-VENT Orchestrator is Online", "docs": "/docs"}
+
+@app.get("/health")
+def health_check():
+
+    return {"status": "healthy"}
